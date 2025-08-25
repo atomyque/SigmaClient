2286885bebb914f1971c8b8b12671df6b866b05b
atomyque <gabyffon@gmail.com>
2025-08-25 14:52:32 +0200

    test

--- features/utils/utils.js
+++ features/utils/utils.js
@@ -4,3 +4,22 @@ export const colors = ["&4", "&c", "&6", "&e", "&2", "&a", "&b", "&3", "&1", "&9
 export function chat(text) {
      ChatLib.chat(`&8[${clickGui.switches["Simplified Name"] == true ? "&f&lΣ&r" : "&f&lSigma &r&fClient"}&8]&r ${text}`)
 }
+
+function preDraw() {
+     GlStateManager.shadeModel(GL_SMOOTH)
+     GlStateManager.enableBlend()
+     GlStateManager.tryBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA)
+     GlStateManager.disableTexture2D()
+     GlStateManager.disableCull()
+     GlStateManager.disableLighting()
+     GlStateManager.disableAlpha()
+}
+
+function postDraw() {
+     GlStateManager.enableTexture2D()
+     GlStateManager.disableBlend()
+     GlStateManager.enableCull()
+     GlStateManager.enableAlpha()
+     GlStateManager.resetColor()
+     GlStateManager.shadeModel(GL_FLAT)
+} // thanks noam
