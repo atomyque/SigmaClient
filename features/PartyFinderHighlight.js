2667e202916ecaf82ab0089e2295ff25450b3cfc
atomyque <gabyffon@gmail.com>
2025-09-20 20:51:20 +0200

    Added manual class selection in party finder highlight, added calculations to search bar

--- features/PartyFinderHighlight.js
+++ features/PartyFinderHighlight.js
@@ -1,29 +1,81 @@
-import { awaitSlots } from "./utils/Server"
 import { Module } from "../gui/ClickGui"
-import { chat } from "./utils/utils"
 
 let parties = []
 let ok = {}
-let ok2 = {}
-let pbparties = []
+let classlist = []
 
-const partyFinderHighlight = new Module("Dungeons", "Party Finder Highlight", "Gives you visual clues in the party finder.").addSwitch("Show Party Size", true)
+//BIG BIG Shoutout to @shaweel for making the search bar make calculations
+
+const partyFinderHighlight = new Module("Dungeons", "Party Finder Highlight", "Gives you visual clues in the party finder.").addSwitch("Show Party Size", true).addSwitch("Manual Class Selection", false)
 const rdrslot = register("renderSlot", slot => {
      if (!partyFinderHighlight.toggled) return
-     if (slot.getInventory().getName() !== "Party Finder") {
-          return
-     }
-     const found = parties.includes(slot.getIndex())
-     if (found) {
-          Renderer.drawRect(Renderer.color(0, 255, 0, 100), slot.getDisplayX(), slot.getDisplayY(), 16, 16)
-     }
-
+     if (slot.getInventory().getName() !== "Party Finder") return
      if (ok[slot.getIndex()] >= 1 && partyFinderHighlight.switches["Show Party Size"]) {
           Renderer.translate(0, 0, 255)
           Renderer.drawString(ok[slot.getIndex()], slot.getDisplayX() + 11, slot.getDisplayY() + 9, true)
      }
-}).unregister()
 
+     const found = parties.includes(slot.getIndex())
+     if (found) {
+          Renderer.drawRect(Renderer.color(0, 255, 0, 100), slot.getDisplayX(), slot.getDisplayY(), 16, 16)
+     }
+}).unregister()
+const defaultcolor = Renderer.color(20, 20, 20, 255)
+register("guiRender", () => {
+     const container = Player?.getContainer()
+     if (container?.getName() !== "Party Finder" || !partyFinderHighlight.switches["Manual Class Selection"]) return
+     drawButton(9, Renderer.screen.getHeight() / 2 - 40, 18, 18, "Healer")
+     new Item("minecraft:potion").setDamage(16421).draw(10, Renderer.screen.getHeight() / 2 - 40, 1)
+
+     drawButton(9, Renderer.screen.getHeight() / 2 - 20, 18, 18, "Mage")
+     new Item("minecraft:blaze_rod").draw(10, Renderer.screen.getHeight() / 2 - 20, 1)
+
+     drawButton(9, Renderer.screen.getHeight() / 2, 18, 18, "Berserk")
+     new Item("minecraft:iron_sword").draw(10, Renderer.screen.getHeight() / 2, 1)
+
+     drawButton(9, Renderer.screen.getHeight() / 2 + 20, 18, 18, "Archer")
+     new Item("minecraft:bow").draw(10, Renderer.screen.getHeight() / 2 + 20, 1)
+
+     drawButton(9, Renderer.screen.getHeight() / 2 + 40, 18, 18, "Tank")
+     new Item("minecraft:leather_chestplate").draw(10, Renderer.screen.getHeight() / 2 + 40, 1)
+})
+
+function drawButton(x, y, width, height, name) {
+     const color = classlist.includes(name) == true ? (white = Renderer.color(255, 255, 255, 255)) : (darkwhite = Renderer.color(150, 150, 150, 255))
+
+     Renderer.drawRect(defaultcolor, x, y, width, height)
+     Renderer.drawLine(color, x, y, x + width, y, 1, 6)
+     Renderer.drawLine(color, x, y + height - 0.5, x + width, y + height - 0.5, 1, 6)
+     Renderer.drawLine(color, x + 0.5, y, x + 0.5, y + height, 1, 6)
+     Renderer.drawLine(color, x + width - 0.5, y, x + width - 0.5, y + height, 1, 6)
+}
+register("guiMouseClick", (mx, my, mb) => {
+     const container = Player?.getContainer()
+     if (container?.getName() !== "Party Finder" || !partyFinderHighlight.switches["Manual Class Selection"]) return
+     if (hovering(mx, my, 10, Renderer.screen.getHeight() / 2 - 40, 18, 18)) toggleClass("Healer")
+     if (hovering(mx, my, 10, Renderer.screen.getHeight() / 2 - 20, 18, 18)) toggleClass("Mage")
+     if (hovering(mx, my, 10, Renderer.screen.getHeight() / 2, 18, 18)) toggleClass("Berserk")
+     if (hovering(mx, my, 10, Renderer.screen.getHeight() / 2 + 20, 18, 18)) toggleClass("Archer")
+     if (hovering(mx, my, 10, Renderer.screen.getHeight() / 2 + 40, 18, 18)) toggleClass("Tank")
+})
+function toggleClass(classname) {
+     if (classlist.includes(classname)) {
+          classlist = classlist.filter(item => item !== classname)
+          Client.scheduleTask(2, () => {
+               fetchParties()
+          })
+          return
+     }
+     classlist.push(classname)
+     Client.scheduleTask(2, () => {
+          fetchParties()
+     })
+}
+
+function hovering(mx, my, x, y, width, height) {
+     if (mx <= x + width && mx >= x && my <= y + height && my >= y) return true
+     else return false
+}
 let pfclass = "Berserker"
 register("packetReceived", packet => {
      if (!partyFinderHighlight.toggled) return
@@ -41,119 +93,42 @@ register("packetReceived", packet => {
                }
           })
      }
-     if (packet.func_179840_c().func_150254_d() == "§rParty Finder§r") {
-          rdrslot.register()
-          Client.scheduleTask(2, () => {
-               //    Client.scheduleTask(3, () => {
-               const container = Player.getContainer()
-               parties = []
-               pbparties = []
-               ok = {}
-               for (let i = 10; i < 34; i++) {
-                    if (container.getStackInSlot(i) !== null && container.getStackInSlot(i).getName().includes("Party")) {
-                         let hasclass = false
-                         container
-                              .getStackInSlot(i)
-                              .getLore()
-                              .slice(7, 12)
-                              .forEach(s => {
-                                   if (s.endsWith(")")) {
-                                        if (ok[i] >= 1) ok[i]++
-                                        else ok[i] = 1
-                                   }
-                                   if (s.includes(`§e${pfclass}§b`)) {
-                                        hasclass = true
-                                        return
-                                   }
-                              })
-
-                         if (!hasclass) {
-                              parties.push(i)
-                         }
-                    }
-               }
-               //    })
-          })
-     }
+     if (!packet.func_179840_c().func_150254_d() == "§rParty Finder§r") return
+     rdrslot.register()
+     Client.scheduleTask(2, () => {
+          fetchParties()
+     })
 }).setFilteredClass(net.minecraft.network.play.server.S2DPacketOpenWindow)
 
-// register("packetReceived", packet => {
-//      if (!partyFinderHighlight.toggled) return
-
-//      if (packet.func_179840_c().func_150254_d() == "§rParty Finder§r") {
-//           pbparties = []
-
-//           rdrslot.register()
-//           Client.scheduleTask(2, () => {
-//                for (let i = 10; i < 34; i++) {
-//                     const container = Player.getContainer()
-//                     let partytime = 0
-//                     ChatLib.chat(i)
-//                     const match0 = container
-//                          .getStackInSlot(i)
-//                          .getLore()[3]
-//                          .match(/§5§o§7§7Note: §f(.+)$/)
-//                     if (match0) {
-//                          //   ChatLib.chat(match0[1])
-//                          if (match0[1].includes(":")) {
-//                               const matchsmc = match0[1].match(/(\d+):(\d+)/)
-//                               if (matchsmc) {
-//                                    partytime = parseInt(matchsmc[1] + matchsmc[2])
-//                               }
-//                          } else {
-//                               const match1 = match0[1].match(/(\d+)/)
-//                               if (match1) {
-//                                    partytime = parseInt(match1[1])
-//                               }
-//                          }
-//                          if (partytime > 300 || partytime < 600) {
-//                               pbparties.push(i)
-//                          }
-//                     }
-//                }
-//           })
-//      }
-// }).setFilteredClass(net.minecraft.network.play.server.S2DPacketOpenWindow)
-
-// register("guiOpened", () => {
-//      // if (!Settings.PartyFinderOutline) return
-//      setTimeout(() => {
-//           pboutline.register()
-//      }, 1)
-// })
-// register("guiClosed", () => {
-//      pboutline.unregister()
-// })
-
-// const pboutline = register("renderSlot", slot => {
-//      if (slot.getItem() === null) return
-//      if (slot.getInventory().getName() !== "Party Finder") return
-//      if (slot.getIndex() < 10 || slot.getIndex() > 34) return
-
-//      if (!slot.getItem().getName().includes("Party")) return
-//      let partytime = null
-//      const match0 = slot
-//           .getItem()
-//           .getLore()[3]
-//           .match(/§5§o§7§7Note: §f(.+)$/)
-//      if (match0) {
-//           //   ChatLib.chat(match0[1])
-//           if (match0[1].includes(":")) {
-//                const matchsmc = match0[1].match(/(\d+):(\d+)/)
-//                if (matchsmc) {
-//                     partytime = parseInt(matchsmc[1] + matchsmc[2])
-//                }
-//           } else {
-//                const match1 = match0[1].match(/(\d+)/)
-//                if (match1) {
-//                     partytime = parseInt(match1[1])
-//                }
-//           }
-//           if (partytime < 100 || partytime > Settings.PartyFinderOutlineRequirement) return
-//           const color = Renderer.color(0, 255, 0, 255)
-//           Renderer.drawRect(color, slot.getDisplayX() - 1, slot.getDisplayY() - 1, 18, 1)
-//           Renderer.drawRect(color, slot.getDisplayX() - 1, slot.getDisplayY() + 16, 18, 1)
-//           Renderer.drawRect(color, slot.getDisplayX() - 1, slot.getDisplayY() - 1, 1, 18)
-//           Renderer.drawRect(color, slot.getDisplayX() + 16, slot.getDisplayY() - 1, 1, 18)
-//      }
-// }).unregister()
+function fetchParties() {
+     const container = Player?.getContainer()
+     parties = []
+     pbparties = []
+     ok = {}
+     for (let i = 10; i < 35; i++) {
+          if (!container?.getStackInSlot(i)?.getName()?.includes("Party")) continue
+          let hasclass = false
+          let found = []
+          container
+               ?.getStackInSlot(i)
+               ?.getLore()
+               ?.slice(7, 12)
+               ?.forEach(s => {
+                    if (s.endsWith(")")) {
+                         if (ok[i] >= 1) ok[i]++
+                         else ok[i] = 1
+                    }
+                    if (s.includes(`§e${pfclass}§b`) && !partyFinderHighlight.switches["Manual Class Selection"]) {
+                         hasclass = true
+                         return
+                    }
+                    if (partyFinderHighlight.switches["Manual Class Selection"]) {
+                         classlist.forEach(classname => {
+                              if (s.includes(classname) && !found.includes(classname)) found.push(classname)
+                         })
+                    }
+               })
+          if (partyFinderHighlight.switches["Manual Class Selection"] && found.length != classlist.length) parties.push(i)
+          if (!partyFinderHighlight.switches["Manual Class Selection"] && !hasclass) parties.push(i)
+     }
+}
--- features/SearchBar.js
+++ features/SearchBar.js
@@ -3,11 +3,98 @@ import { guiPiece } from "../gui/draggableGuis"
 const defaultcolor = Renderer.color(20, 20, 20, 255)
 
 let typedtext = ""
+let showntext = ""
+let lasttyped = 0
 
 const keyboard = Java.type("org.lwjgl.input.Keyboard")
 const SearchBar = new Module("Misc", "Search Bar", "Highlights the items containing what you put in the search bar.").addSwitch("Double Click To Highlight").addColor("Highlight Color", 0, 225, 0, 150, true)
 let highlight = false
 
+function formatLargeNumber(num) {
+     sign = Math.sign(num)
+     num = Math.abs(num)
+     num = String(num)
+     numLen = num.length
+     if (numLen < 4) return num
+     itter = 0
+     numArray = []
+     for (char of num) {
+          numArray.push(char)
+     }
+     amount = 0
+     for (char of num) {
+          itter += 1
+          if (numLen % 3 == itter % 3 && itter != numLen) {
+               numArray.splice(itter + amount, 0, ",")
+               amount++
+          }
+     }
+     if (sign == -1) {
+          num = "-"
+     } else {
+          num = ""
+     }
+     for (char of numArray) {
+          num += char
+     }
+     return num
+}
+
+function isNum(n) {
+     n.replaceAll("*", "")
+     n.replaceAll("/", "")
+     n.replaceAll("+", "")
+     n.replaceAll("-", "")
+     if (n.endsWith("k")) {
+          n = n.split("k")[0]
+     }
+     if (n.endsWith("m")) {
+          n = n.split("m")[0]
+     }
+     if (n.endsWith("b")) {
+          n = n.split("b")[0]
+     }
+     if (n.endsWith("t")) {
+          n = n.split("t")[0]
+     }
+     if (Number(n) == NaN) return false
+     return true
+}
+
+function calculate() {
+     try {
+          calc = typedtext
+          calcSplitted = calc.split(/([+\-*/])/)
+          calc = ""
+          for (num of calcSplitted) {
+               if (num.endsWith("k") && isNum(num)) {
+                    num = "(" + num.replace("k", "*1000)")
+               }
+               if (num.endsWith("m") && isNum(num)) {
+                    num = "(" + num.replace("m", "*1000000)")
+               }
+               if (num.endsWith("b") && isNum(num)) {
+                    num = "(" + num.replace("b", "*1000000000)")
+               }
+               if (num.endsWith("t") && isNum(num)) {
+                    num = "(" + num.replace("t", "*1000000000000)")
+               }
+               calc += num
+          }
+          ans = "&a = " + formatLargeNumber(eval(calc))
+          if (ans != "&a = NaN") {
+               showntext = typedtext
+               showntext += ans
+               return
+          }
+          if (ans == "&a = NaN") {
+               showntext = typedtext
+          }
+     } catch (error) {
+          showntext = typedtext
+          return
+     }
+}
 const type = register("guiKey", (char, keycode, gui, event) => {
      if (!SearchBar.toggled) return
      const screen = Client.getMinecraft().field_71462_r
@@ -20,21 +107,46 @@ const type = register("guiKey", (char, keycode, gui, event) => {
      if (keycode == 28) {
           type.unregister()
 
+          calculate()
           return
      }
      if (keycode == 14 && !keyboard.isKeyDown(keyboard.KEY_LCONTROL)) {
           typedtext = typedtext.slice(0, -1)
+          let first = true
+          const clickeddate = Date.now()
+          const remove = register("step", () => {
+               if (Date.now() - clickeddate < 750) return
+               if (first) return (first = false)
+               if (!keyboard.isKeyDown(keyboard.KEY_BACK)) return remove.unregister()
+               typedtext = typedtext.slice(0, -1)
+               calculate()
+          }).setFps(15)
 
+          calculate()
           return
      }
      if (keycode == 14 && keyboard.isKeyDown(keyboard.KEY_LCONTROL)) {
           typedtext = typedtext.trim().split(" ").slice(0, -1).join(" ") + " "
 
+          calculate()
           return
      }
      if (!/^[\x20-\x7E]*$/.test(char)) return
+
+     let first = true
+     const clickeddate = Date.now()
+     lasttyped = clickeddate
+
+     const remove = register("step", () => {
+          if (Date.now() - clickeddate < 750) return
+          if (first) return (first = false)
+          if (!keyboard.isKeyDown(eval("keyboard." + "KEY_" + keyboard.getKeyName(keycode))) || lasttyped != clickeddate) return remove.unregister()
+          typedtext += char
+          calculate()
+     }).setFps(15)
      typedtext += char
      cancel(event)
+     calculate()
 }).unregister()
 
 register("guiClosed", () => {
@@ -47,21 +159,20 @@ register("guiClosed", () => {
 
 let lastclick = 0
 
-register("renderItemOverlayIntoGui", (item, x, y, event) => {
-     if (!SearchBar.toggled) return
-     if (!highlight && SearchBar.switches["Double Click To Highlight"]) return
-     if (!item.getName().toLowerCase().includes(typedtext.trim()) || typedtext.trim() == "") return
-     const screen = Client.getMinecraft().field_71462_r
-     let found = false
-     guis.forEach(guiname => {
-          if (!screen?.toString()?.toLowerCase()?.includes(guiname)) return
-          found = true
-     })
-     if (!found) return
+// register("renderItemOverlayIntoGui", (item, x, y, event) => {
+//      if (!SearchBar.toggled) return
+//      if (!highlight && SearchBar.switches["Double Click To Highlight"]) return
+//      if (!item.getName().toLowerCase().includes(typedtext.trim()) || typedtext.trim() == "") return
+//      const screen = Client.getMinecraft().field_71462_r
+//      let found = false
+//      guis.forEach(guiname => {
+//           if (!screen?.toString()?.toLowerCase()?.includes(guiname)) return
+//           found = true
+//      })
+//      if (!found) return
 
-     const color = Renderer.color(SearchBar.color["Highlight Color"].r, SearchBar.color["Highlight Color"].g, SearchBar.color["Highlight Color"].b, SearchBar.color["Highlight Color"].alpha)
-     Renderer.drawRect(color, x, y, 16, 16)
-})
+//      const color = Renderer.color(SearchBar.color["Highlight Color"].r, SearchBar.color["Highlight Color"].g, SearchBar.color["Highlight Color"].b, SearchBar.color["Highlight Color"].alpha)
+// })
 
 const guis = ["inventory", "container", "chest"]
 
@@ -76,6 +187,7 @@ const bordercolor = () => {
      return white
 }
 
+const width = 200
 register("postGuiRender", () => {
      if (!SearchBar.toggled) return
      y = Renderer.screen.getHeight() - 25
@@ -88,18 +200,18 @@ register("postGuiRender", () => {
      if (!found) return
 
      let minus = 0
-     while (Renderer.getStringWidth(typedtext.trim().slice(0, typedtext.trim().length - minus)) > 144) {
+     while (Renderer.getStringWidth(typedtext.trim().slice(0, typedtext.trim().length - minus)) > width - 6) {
           minus++
           bar = false
      }
      Renderer.retainTransforms(true)
-     Renderer.drawRect(defaultcolor, Renderer.screen.getWidth() / 2 - 75, y, 150, 20)
-     Renderer.drawLine(bordercolor(), Renderer.screen.getWidth() / 2 - 75, y, Renderer.screen.getWidth() / 2 + 75, y, 1, 6)
-     Renderer.drawLine(bordercolor(), Renderer.screen.getWidth() / 2 - 75, y + 19.5, Renderer.screen.getWidth() / 2 + 75, y + 19.5, 1, 6)
-     Renderer.drawLine(bordercolor(), Renderer.screen.getWidth() / 2 - 74.5, y, Renderer.screen.getWidth() / 2 - 74.5, y + 20, 1, 6)
-     Renderer.drawLine(bordercolor(), Renderer.screen.getWidth() / 2 + 74.5, y, Renderer.screen.getWidth() / 2 + 74.5, y + 20, 1, 6)
-     Renderer.drawString(minus > 0 ? typedtext.trim().slice(0, typedtext.trim().length - minus - 1) + "..." : typedtext.trim(), Renderer.screen.getWidth() / 2 - 72, y + 5.5, true)
-     if (bar) Renderer.drawRect(Renderer.WHITE, Renderer.screen.getWidth() / 2 - 72 + Renderer.getStringWidth(typedtext.trim()), y + 3.5, 1, 12)
+     Renderer.drawRect(defaultcolor, Renderer.screen.getWidth() / 2 - width / 2, y, width, 20)
+     Renderer.drawLine(bordercolor(), Renderer.screen.getWidth() / 2 - width / 2, y, Renderer.screen.getWidth() / 2 + width / 2, y, 1, 6)
+     Renderer.drawLine(bordercolor(), Renderer.screen.getWidth() / 2 - width / 2, y + 19.5, Renderer.screen.getWidth() / 2 + width / 2, y + 19.5, 1, 6)
+     Renderer.drawLine(bordercolor(), Renderer.screen.getWidth() / 2 - (width / 2 - 0.5), y, Renderer.screen.getWidth() / 2 - (width / 2 - 0.5), y + 20, 1, 6)
+     Renderer.drawLine(bordercolor(), Renderer.screen.getWidth() / 2 + (width / 2 - 0.5), y, Renderer.screen.getWidth() / 2 + (width / 2 - 0.5), y + 20, 1, 6)
+     Renderer.drawString(minus > 0 ? showntext.slice(0, showntext.length - minus - 1) + "..." : showntext, Renderer.screen.getWidth() / 2 - width / 2 + 3, y + 5.5, true)
+     if (bar) Renderer.drawRect(Renderer.WHITE, Renderer.screen.getWidth() / 2 - width / 2 + 3 + Renderer.getStringWidth(typedtext), y + 3.5, 1, 12)
      Renderer.retainTransforms(false)
 })
 register("guiMouseClick", (mx, my, mb, gui) => {
@@ -113,8 +225,8 @@ register("guiMouseClick", (mx, my, mb, gui) => {
      })
      if (!found) return
 
-     if (hovering(mx, my, Renderer.screen.getWidth() / 2 - 75, y, 150, 20) && mb == 0) {
-          if (Date.now() - lastclick < 750) {
+     if (hovering(mx, my, Renderer.screen.getWidth() / 2 - width / 2, y, 150, 20) && mb == 0) {
+          if (Date.now() - lastclick < width / 20) {
                highlight = !highlight
                lastclick = 0
           } else lastclick = Date.now()
@@ -122,10 +234,10 @@ register("guiMouseClick", (mx, my, mb, gui) => {
           bar = true
           type.register()
      }
-     if (hovering(mx, my, Renderer.screen.getWidth() / 2 - 75, y, 150, 20) && mb == 1) {
+     if (hovering(mx, my, Renderer.screen.getWidth() / 2 - width / 2, y, 150, 20) && mb == 1) {
           typedtext = ""
      }
-     if (!hovering(mx, my, Renderer.screen.getWidth() / 2 - 75, y, 150, 20)) {
+     if (!hovering(mx, my, Renderer.screen.getWidth() / 2 - width / 2, y, 150, 20)) {
           type.unregister()
           blinkingbar.unregister()
           bar = false
